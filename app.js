6f54831b084c2845122a7e21168bd3dcab959d6e
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-01-16 11:28:52 +0100

    Added an object

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 const arr = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(arr);
+const newChar = {
+    name: "Benny",
+    age: 47,
+    superhero: true,
+};
+console.log(newChar);
--- app.ts
+++ app.ts
@@ -1,2 +1,16 @@
 const arr: string[] = ["apa", "orangutang", "gorilla", "get", "giraff"]
 console.log(arr);
+
+type Test = {
+    name: string,
+    age: number,
+    superhero: boolean,
+}
+
+const newChar: Test = {
+    name: "Benny",
+    age: 47,
+    superhero: true,
+}
+
+console.log(newChar);
